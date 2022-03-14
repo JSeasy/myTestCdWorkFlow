4e910824edcdd73a66813cf396a868e96a4ede10
гений <603850549@qq.com>
2022-03-14 11:23:29 +0800

    外包项目

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import styles from './index.less';
+export default () => {
+  return (
+    <>
+      <div className={styles.login}>
+        <div className={styles.wrap}></div>
+      </div>
+    </>
+  );
+};
---
+++ src/pages/onePage/index.tsx
@@ -0,0 +1,8 @@
+
+export default function o() {
+  return (
+    <div>
+      <h1>Page two</h1>
+    </div>
+  );
+}
